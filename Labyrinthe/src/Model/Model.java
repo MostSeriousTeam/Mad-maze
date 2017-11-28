b3fe7e2c9cfea43c3d184dcf593c315bad38ca01
Lydia Zekrini <lzekrini@emi.u-bordeaux1.fr>
2017-11-28 14:35:24 +0100

    ajout de classes actor et move

---
+++ Labyrinthe/src/Model/Model.java
@@ -0,0 +1,14 @@
+package Model;
+
+
+public class Model {
+	private static Model instance;
+
+	public static Model getInstance() {
+		if (instance == null) {
+			instance = new Model();
+		}
+		return instance;
+	}
+
+}
